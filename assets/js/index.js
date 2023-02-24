170b9a7cf3f00a09998331acb474b2d8d727150e
swmac <shaynefw@gmail.com>
2023-02-24 08:57:21 -0500

    init commit

---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  try {event.preventDefault(); // prevent the default form submission
+
+  // Get the user input from the form
+  const repoName = document.querySelector("#repoName").value;
+  const githubUsername = document.querySelector("#githubUsername").value;
+  const fileName = document.querySelector("#fileName").value;
+
+  // Create the markdown content based on the user input
+  const data = {
+    repoName: repoName,
+    githubUsername: githubUsername,
+  };
+
+  const fileContent = generateREADME(data);
+
+  // Create a blob object from the file content
+  const blob = new Blob([fileContent], { type: "text/markdown" });
+
+  // Create a temporary URL for the blob object
+  const url = URL.createObjectURL(blob);
+
+  // Create an anchor element with a download attribute and click it
+  //if the user adds .md to the file name, alert them that it is not needed
+  if (fileName.includes(".md")) {
+    alert(" '.md' is not needed in the file name.")
+    return;
+  } else {
+  const link = document.createElement("a");
+  link.download = `${fileName}.md`;
+  link.href = url;
+  link.style.display = "none";
+  document.body.appendChild(link);
+  link.click();
+  alert("README.md has been created successfully!");
+  }
+
+
+  // Wait for the file to be downloaded, then revoke the URL and remove the anchor element
+  setTimeout(() => {
+    URL.revokeObjectURL(url);
+    document.body.removeChild(link);
+  }, 0);
+  } catch (error) {
+    console.log(error);
+  }
+});
---
+++ assets/js/lib/genMD.js
@@ -0,0 +1,47 @@
+// This file contains the function that generates the README.md file
+function generateREADME(data) {
+  return `# ${data.repoName}
+      
+  ## Description
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  
+  ## Installation
+  
+  - [Project repo](https://github.com/${data.githubUsername}/${data.repoName})
+  - [Project deployed link](https://${data.githubUsername}.github.io/${data.repoName}/)
+  - [Project Video Demo]()
+  
+  ## Usage
+  
+  ![screenShot]()
+  
+  ## Credits
+  
+  ## License
+  
+  ## Badges
+  
+  ![HTML](https://img.shields.io/badge/HTML-NUMBER%25-orange)
+  ![CSS](https://img.shields.io/badge/CSS-NUMBER%25-blue)
+  ![JavaScript](https://img.shields.io/badge/JavaScript-NUMBER%25-yellow)
+  
+  ## Features
+  
+  ## How to Contribute
+  
+  ## Tests
+  
+  ## Questions
+  
+  If you have any questions you can reach me by email at through my [github profile](https://github.com/${data.githubUsername}).
+  `;
+}
+
+// Export the function so it can be used in other files
+window.generateREADME = generateREADME;
